29304c9cdced02ea3326a9c551b11d109a5a988a
Adriano Camargo <adriano@grupoviabrasil.net.br>
2022-08-29 20:46:11 -0300

    Fiz Objetos: Carro,Pessoa e Barco

---
+++ src/poo/Barco.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author adriano.camargo
+ */
+public class Barco {
+
+    String cor;
+    String nome;
+    Double tamanho;
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double gettamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(Double tamanho) {
+        this.tamanho = tamanho;
+    }
+
+}
---
+++ src/poo/Pessoa.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author adriano.camargo
+ */
+public class Pessoa {
+
+    String nome;
+    String corDoCabelo;
+    String bioTipo;
+    int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getcorDoCabelo() {
+        return corDoCabelo;
+    }
+
+    public void setcorDoCabelo(String corDoCabelo) {
+        this.corDoCabelo = corDoCabelo;
+    }
+
+    public String bioTipo() {
+        return bioTipo;
+    }
+
+    public void setbioTipo(String bioTipo) {
+        this.bioTipo = bioTipo;
+    }
+
+    public int getidade() {
+        return idade;
+    }
+
+    public void setidade(int idade) {
+        this.idade = idade;
+    }
+
+}
