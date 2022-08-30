c09156e098c44f42015af5fb667f48cca69736e4
Adriano Camargo <adriano@grupoviabrasil.net.br>
2022-08-29 21:28:24 -0300

    Altecao void

--- src/poo/poo.java
+++ src/poo/poo.java
@@ -9,13 +9,24 @@ package poo;
  *
  * @author adriano.camargo
  */
-public class POO {
+public class poo {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Carro meuCarro = new Carro();
+        
+        meuCarro.setCor("Preto");
+        meuCarro.setPlaca("MMM0253");
+        meuCarro.setTipo("Gol");
+        meuCarro.setnumPortas(4);
+        System.out.println("---------Carro------");
+        System.out.println("Cor "+meuCarro.getCor());
+        System.out.println("Nº Portas "+meuCarro.getnumPortas());
+        System.out.println("Placa "+meuCarro.getnumPortas());
+
     }
     
 }
